c4c82f7d19674a05004221489a1b532a4ad55c11
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-16 12:59:17 -0400

    Update Sum_Zero.java

--- Sum_Zero/src/Sum_Zero/Sum_Zero.java
+++ Sum_Zero/src/Sum_Zero/Sum_Zero.java
@@ -2,32 +2,39 @@ package Sum_Zero;
 
 import java.util.*;
 
+/*This class is the implementation class for the output zero from the sum of the three elements in the array.
+  The user input values will be inserted by the main class.
+  The implementation will use the binary search in order to find the new array and three elements whose sum becomes zero.
+*/
+
 public class Sum_Zero {
+    
     public List<List<Integer>> Sum_Zero(int[] userInputs){
 
-        Arrays.sort(userInputs);
-        Set<List<Integer>> output = new HashSet<>();
+        Arrays.sort(userInputs); //Before the binary search, we need to sort the input array.
+        Set<List<Integer>> output = new HashSet<>(); //Initialize the new set for storing the new output arrays.
 
-        if(userInputs.length==0 || userInputs.length==1 || userInputs==2){
+        if(userInputs.length==0 || userInputs.length==1 || userInputs==2){ //If the length is less than three, then more input values will be needed.
             System.out.println("There would need more input values from the user.");
             return Collections.emptyList();
         }
 
+        //In order to find the sum zero array, we need to consider three cases in total.
         for(int fixed=0; fixed<userInputs.length; fixed++){
             int smallest = fixed+1;
             int largest = userInputs.length-1;
             int sum_two = -userInputs[fixed];
 
             while(smallest<largest){
-                if(userInputs[largest]+userInputs[smallest]==sum_two){
-                    output.add(Arrays.asList(userInputs[fixed],userInputs[smallest],userInputs[largest]));
+                if(userInputs[largest]+userInputs[smallest]==sum_two){ //case 1: when the output meets the sum of zero from the three elements,
+                    output.add(Arrays.asList(userInputs[fixed],userInputs[smallest],userInputs[largest])); // then we add this array to the new set to store our results.
                     largest = largest -1;
                     smallest = smallest + 1;
                 }
-                if(userInputs[smallest]+userInputs[largest]<sum_two){
+                if(userInputs[smallest]+userInputs[largest]<sum_two){ //case 2: 
                     smallest = smallest + 1;
                 }
-                if(userInputs[smallest]+userInputs[largest]>sum_two){
+                if(userInputs[smallest]+userInputs[largest]>sum_two){ //case 3:
                     largest = largest -1;
                 }
             }
@@ -35,4 +42,4 @@ public class Sum_Zero {
         }
         return new ArrayList(output);
     }
-}
+}//Sum_Zero
