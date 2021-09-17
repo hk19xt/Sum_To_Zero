702e0c511227f418f11e76927f97769f06c1bb52
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-16 23:49:53 -0400

    Update Main.java

--- Sum_Zero/src/Sum_Zero/Main.java
+++ Sum_Zero/src/Sum_Zero/Main.java
@@ -1,7 +1,14 @@
 package Sum_Zero;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        
+        int[] userInputs = {-1,0,1,2,-1,4};
+        Sum_Zero SZ = new Sum_Zero();
+        List output = new ArrayList();
+        output = SZ.Sum_Zero(userInputs);
+        System.out.println("The output results will be: "+ output);
     }
 }
