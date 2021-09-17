ae85023e371072c9a6a29c99d023795eab631769
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-16 23:52:17 -0400

    Update Main.java

--- Sum_Zero/src/Sum_Zero/Main.java
+++ Sum_Zero/src/Sum_Zero/Main.java
@@ -2,10 +2,12 @@ package Sum_Zero;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/* This class is the main class. The users can change the input values in the hardcoded lines.
+   After compiling the program, the output arrays will be shown in the console.
+*/
 public class Main {
     public static void main(String[] args){
-        int[] userInputs = {-1,0,1,2,-1,4};
+        int[] userInputs = {-3,0,2,1,1,-2};
         Sum_Zero SZ = new Sum_Zero();
         List output = new ArrayList();
         output = SZ.Sum_Zero(userInputs);
